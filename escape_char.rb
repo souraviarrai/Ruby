0bb5f192914fd61800143f23be31932d114d6bf0
souravdungbungrai <souravdungbungrai@gmail.com>
2023-04-14 01:16:46 +0600

    first

---
+++ escape_char.rb
@@ -0,0 +1,4 @@
+puts "some text\nmore text"
+puts "\tHello ruby"
+puts "juliet said \"goodbye\" to me."
+puts 'sourav said "hello" to ruby'
---
+++ puts_method.rb
@@ -0,0 +1,7 @@
+puts 1
+puts 1 + 2
+puts(1 + 2 + 3)
+
+puts "hello sourav from ruby"
+puts
+puts 1, 2, 3
