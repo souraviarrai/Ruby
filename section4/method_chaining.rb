0a02c2bd1ee70982ba2f78c0594c3a24c58d509c
souravdungbungrai <souravdungbungrai@gmail.com>
2023-04-14 13:39:54 +0600

    method

---
+++ section4/method_chaining.rb
@@ -0,0 +1,15 @@
+puts "hi there".upcase.length.succ
+puts 10.next.next.pred
+
+#inspect method
+
+puts "killer is free".inspect
+puts 8.inspect
+
+# nill object
+
+# wednesday = nil
+# puts wednesday.inspect
+
+greet = puts "hello"
+p greet
