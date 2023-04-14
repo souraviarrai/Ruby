c3bd477b6f259e81858fdf0fbc435254cf651628
souravdungbungrai <souravdungbungrai@gmail.com>
2023-04-14 21:50:53 +0600

    new

---
+++ section4/boolean.rb
@@ -0,0 +1,27 @@
+# boolean
+puts 5 < 10
+puts 5 > 10
+
+# equality operator
+
+# equality ==
+# assignment =
+
+puts 10 == 10
+puts 1 == 2
+puts
+puts "hello" == "hello"
+puts "sourav" == "rai"
+
+# inequality
+puts 10 != 10
+puts 10 != 5
+
+# mathematical comparisons
+# predicate methods
+
+puts 10.odd?
+puts 10.positive?
+puts 11.even?
+
+#
---
+++ section4/method_args.rb
@@ -0,0 +1,18 @@
+puts "Big Mac".include? "B"
+puts "Sourav".include? "S"
+puts "killer".include? "K"
+
+# for multiple arguments
+
+puts 20.between?(10, 30)
+puts 20.between?(10, 15)
+
+# arithmetic method
+
+# float method
+
+puts 1.33.floor.class
+puts 11.333.ceil
+puts 1.22.round
+puts 1.99923.round
+puts -34.54.abs
