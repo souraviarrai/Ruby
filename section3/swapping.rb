eb4df34048180552ea28ba3eb071fe31f6cd312c
souravdungbungrai <souravdungbungrai@gmail.com>
2023-04-14 12:51:43 +0600

    new

---
+++ section3/swapping.rb
@@ -0,0 +1,24 @@
+a, b = 3, 4
+puts a, b
+a, b = b, a
+puts a, b
+
+# assignment short cuts
+
+x = 10
+x += 5
+puts x
+
+y = 300
+y -= 100
+puts y
+
+z = "sourav"
+z += " rai"
+puts z
+
+# constant
+
+Pi = 3.14159
+Pi = 23
+puts Pi
