a10f1445f5ebe6cb98e4f003df9322086d83346f
souravdungbungrai <souravdungbungrai@gmail.com>
2023-04-14 13:27:45 +0600

    sec4

---
+++ section4/method.rb
@@ -0,0 +1,14 @@
+puts "hello world".length
+puts "hello world".upcase
+puts "SOURAV".downcase
+expression = "sourav rai"
+puts expression.length
+
+# integer method .next
+
+puts 99.next()
+puts 0.pred()
+puts -1.pred()
+puts 1.succ()
+
+# exception error
